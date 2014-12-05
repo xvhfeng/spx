a1abce0c0915a075479115983625b111b2599ab3
root <xvhfeng@gmail.com>
2014-12-05 18:52:40 +0800

    check over mmeory

--- src/spx_task_module.c
+++ src/spx_task_module.c
@@ -30,25 +30,25 @@
  struct spx_module_context *g_spx_task_module = NULL;
 
 void spx_task_module_receive_handler(struct ev_loop *loop,ev_io *w,int revents){
-    struct spx_task_context *tcontext = NULL;
+    struct spx_task_context *task = NULL;
     struct spx_receive_context *tc = (struct spx_receive_context *) w;//magic,yeah
     size_t len = 0;
     err_t err= 0;
-    err = spx_read_nb(w->fd,(byte_t *) &tcontext,sizeof(tcontext),&len);
-    if(0 != err || len != sizeof(tcontext)){
+    err = spx_read_nb(w->fd,(byte_t *) &task,sizeof(task),&len);
+    if(0 != err || len != sizeof(task)){
         SpxLog2(tc->log,SpxLogError,err,\
                 "read the dio context is fail.");
         return;
     }
 
-    if (NULL == tcontext) {
+    if (NULL == task) {
         SpxLog1(tc->log,SpxLogError,
                 "read the dio context is fail,"\
                 "tne dio context is null.");
         return;
     }
 
-    tcontext->dio_process_handler(loop,tc->idx,tcontext);
+    task->dio_process_handler(loop,tc->idx,task);
     /*  here no deal the error from dio_process_handler
      *  as we donot know deal flow whether use noblacking or blacking
      *  so, we must deal the error in the handler by yourself
@@ -61,23 +61,23 @@ void spx_task_module_receive_handler(struct ev_loop *loop,ev_io *w,int revents){
 //void spx_task_module_wakeup_handler(struct ev_loop *loop,ev_io *w,int revents){
 void spx_task_module_wakeup_handler(int revents,void *arg){
     err_t err = 0;
-    struct spx_task_context *tcontext = (struct spx_task_context *) arg;
-    struct spx_job_context *jc = tcontext->jcontext;
+    struct spx_task_context *task = (struct spx_task_context *) arg;
+    struct spx_job_context *jc = task->jcontext;
     if((revents & EV_TIMEOUT) || (revents & EV_ERROR)){
-        SpxLog1(tcontext->log,SpxLogError,
+        SpxLog1(task->log,SpxLogError,
                 "wakeup task thread is fail.");
-        spx_task_pool_push(g_spx_task_pool,tcontext);
+        spx_task_pool_push(g_spx_task_pool,task);
         spx_job_pool_push(g_spx_job_pool,jc);
     }
     if(revents & EV_WRITE){
-        SpxLogFmt1(jc->log,SpxLogDebug,"task is well.tc idx:%d",tcontext->idx);
+        SpxLogFmt1(jc->log,SpxLogDebug,"task is well.tc idx:%d",task->idx);
         size_t len = 0;
-        err = spx_write_nb(jc->tc->pipe[1],(byte_t *) &tcontext,sizeof(tcontext),&len);
+        err = spx_write_nb(jc->tc->pipe[1],(byte_t *) &task,sizeof(task),&len);
         if (0 != err || sizeof(jc) != len) {
             SpxLog1(jc->log,SpxLogError,\
-                    "send tcontext to dio thread is fail."\
+                    "send task to dio thread is fail."\
                     "then dispatch notice to nio thread deal.");
-            spx_task_pool_push(g_spx_task_pool,tcontext);
+            spx_task_pool_push(g_spx_task_pool,task);
             jc->err = err;
             jc->moore = SpxNioMooreResponse;
             size_t idx = spx_network_module_wakeup_idx(jc);
